a67348205adf2d97508ecf48637e16f464f80d15
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2024-01-22 23:01:42 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (49.59%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+
+        int count = 0 ;
+
+        for(int i = s.length()-1 ; i >=0 ; i --){
+
+            if(s.charAt(i) != ' '){
+                count++ ;
+            }
+
+            else {
+                break ;
+            }
+        }
+        return  count ;
+    }
+}
