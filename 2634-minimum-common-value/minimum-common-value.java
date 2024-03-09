7fdd431760bbcc972e63cc068203e5e66a107691
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2024-03-09 11:00:36 +0530

    Time: 16 ms (14.22%) | Memory: 65.6 MB (5.26%) - LeetSync

---
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        
+        for (int val : nums1) {
+            set1.add(val);
+        }
+        
+        for (int val : nums2) {
+            if (set1.contains(val)) {
+                set2.add(val);
+            }
+        }
+
+        return set2.isEmpty() ? -1 : set2.stream().min(Integer::compareTo).orElseThrow();
+    }
+}
