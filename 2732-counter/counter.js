7b8e0a33ac01fe82deb2a87fae179f970a388434
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2024-03-16 10:09:28 +0530

    Time: 56 ms (23.80%) | Memory: 49.1 MB (15.94%) - LeetSync

--- 2732-counter/counter.js
+++ 2732-counter/counter.js
@@ -3,10 +3,8 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-  return ()=> n++
-
-    return function() {
-        
+  return function() {
+       return n++
     };
 };
 
