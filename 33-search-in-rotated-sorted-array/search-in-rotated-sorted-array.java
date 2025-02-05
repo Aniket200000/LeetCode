366c9d5feaf8f512571d22929475fa386aa97430
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-02-05 20:58:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (40.04%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -3,28 +3,30 @@ class Solution {
         int low = 0 , high = a.length-1, mid=0;
 
         while(low <= high){
-            mid = low + (high-low)/2;
+            mid = low + (high-low)/2; // Avoid overflow
 
-            if(a[mid] == target){
-                return mid;
+           if(a[mid] == target){////Target found at mid
+            return mid;
+           }
+
+           else if(a[low] <= a[mid]) { // Checks if left Half is sorted
+            if(a[low] <= target && target <= a[mid]){ // target in left half
+                high = mid -1 ;
             }
-           else if (a[low] <= a[mid]){
-            if(a[low] <= target && target <= a[mid]){
-                high = mid - 1;
-            }
-            else{
+            else{ // target in right half
                 low = mid + 1;
             }
            }
-           else{
-            if(a[mid] <= target && target <= a[high]){
-                low = mid + 1;
+            else{ // Checks if right half is sorted
+                if(a[mid] < target && target <= a[high]){ // target lies in right half
+                    low = mid + 1;
+                }
+                else { // target lies in left half
+                    high = mid -1;
+                }
             }
-            else{
-                high = mid -1;
-            }
-           }
-        }
-        return -1;
+           
+        } 
+        return -1;// target not found
     }
 }
