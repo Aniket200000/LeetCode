5a186090d7b4be25398d4aeae8a3a0b3ab01e257
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-02-13 14:20:36 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (66.88%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -23,9 +23,8 @@ class Solution {
     }
 
     private boolean isMirror(TreeNode left , TreeNode right){
-        if ( left == null && right == null) return true;
-        if ( left == null || right == null) return false;
-
-        return (left.val == right.val) && isMirror(left.left , right.right) && isMirror(left.right, right.left);
+        if(left == null && right == null) return true;
+        if(left == null || right == null) return false;
+        return (left.val == right.val) && isMirror(left.left , right.right) && isMirror(left.right , right.left);
     }
 }
