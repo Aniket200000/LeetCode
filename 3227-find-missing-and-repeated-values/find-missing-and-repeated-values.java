d33caf7ba7d87f234087327dffc6b12999f711d1
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-03-06 12:33:25 +0530

    Time: 4 ms (47.89%) | Memory: 45 MB (99.50%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.java
@@ -1,34 +1,23 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int n = grid.length;
-        int last = n * n;
-        ArrayList<Integer> nums = new ArrayList<>();
+    int n = grid.length;
+    int last = n * n;
+    HashSet<Integer> seen = new HashSet<>();
+    int duplicate = -1, missing = -1;
 
-        for (int i = 0; i < grid.length; i++) {  
-            for (int j = 0; j < grid[i].length; j++) {  
-                nums.add(grid[i][j]);
-            }
+    for (int[] row : grid) {
+        for (int num : row) {
+            if (!seen.add(num)) duplicate = num; 
         }
-
-        nums.sort(null);  
-
-        int[] arr = new int[2];
-
-        for (int k = 1; k < nums.size(); k++) {  
-            if (nums.get(k).equals(nums.get(k - 1))) {
-                arr[0] = nums.get(k);  
-                break;  
-            }
-        }
-
-        for (int k = 1; k <= last; k++) {  
-            if (!nums.contains(k)) {  
-                arr[1] = k;
-                break;
-            }
-        }
-
-        return arr;
     }
 
+    for (int k = 1; k <= last; k++) {
+        if (!seen.contains(k)) {
+            missing = k;
+            break;
+        }
+    }
+
+    return new int[]{duplicate, missing};
+}
 }
