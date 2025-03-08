7578648e4c477dba5e2de990d9d3cf2ae532d37f
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-03-08 13:29:21 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (58.45%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -3,25 +3,26 @@ class Solution {
         char[] charArr = blocks.toCharArray();
         int n = charArr.length;
 
-        int Wcount = 0, change = 0;
-        
-        for (int i = 0; i < k; i++) {
-            if (charArr[i] == 'W') { // Counts White
+        int Wcount = 0 , change ;
+
+        for(int i = 0 ; i < k ; i++){
+            if(charArr[i] == 'W'){
                 Wcount++;
             }
         }
         change = Wcount;
 
-        for (int right = k; right < n; right++) {
-            if (charArr[right - k] == 'W') {
+        for(int right = k ; right < n ; right++){
+            if(charArr[right - k] == 'W'){
                 Wcount--;
             }
-            if (charArr[right] == 'W') {
+            if(charArr[right] == 'W'){
                 Wcount++;
             }
-            change = Math.min(change, Wcount);
-        }
 
+            change = Math.min(change , Wcount);
+
+        }
         return change;
     }
 }
