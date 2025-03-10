23831488893e215b67b1e9d05ec8701d49234701
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-03-10 19:50:06 +0530

    Time: 5 ms (61.28%) | Memory: 63.3 MB (28.69%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.java
+++ 3483-alternating-groups-ii/alternating-groups-ii.java
@@ -1,18 +1,18 @@
 class Solution {
     public int numberOfAlternatingGroups(int[] colors, int k) {
-        int groups = 0;
+        int groups = 0 ;
         int n = colors.length;
         int left = 0;
 
-        //Skipping the entire subarray
-        for (int right = 1; right < (n + k - 1); right++) {// expanding the window
-            if (colors[right % n] == colors[(right - 1) % n]) {
-                left = right;
+        for(int right = 1 ; right < n + k - 1 ; right++ ){ // Expanding the window by moving right
+
+            // if alternating elements become same then skip the entire subarray
+            if(colors[right % n] == colors[(right - 1)% n]){
+                left = right ; 
             }
 
-            //shrinking the window
-            if (right - left + 1 == k) {
-                left++;
+            if(right - left + 1 == k){
+                left++;  // Shrinking the window by moving left
                 groups++;
             }
         }
