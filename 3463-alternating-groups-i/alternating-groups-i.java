5a60fe736c4da8134acf6b074b41f122201b983f
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-03-12 20:07:41 +0530

    Time: 1 ms (97.57%) | Memory: 43.3 MB (34.07%) - LeetSync

--- 3463-alternating-groups-i/alternating-groups-i.java
+++ 3463-alternating-groups-i/alternating-groups-i.java
@@ -2,14 +2,14 @@ class Solution {
     public int numberOfAlternatingGroups(int[] colors) {
         int groups = 0 ;
         int n = colors.length;
-        for (int i = 0; i < n; i++) {
-            int left = colors[(i - 1 + n) % n];   // Left neighbor (circular)
-            int mid = colors[i];                  // Current element
-            int right = colors[(i + 1) % n];      // Right neighbor (circular)
+        
 
-            // Check if the middle element is different from both neighbors
-            if (mid != left && mid != right) {
-                groups++;
+        for(int i = 0 ;  i < n ; i++ ){ // Expanding the window by moving right
+
+            if(colors[i % n] != colors[(i - 1 + n)% n]){
+                if(colors[i % n] != colors[(i + 1)% n]){
+                    groups++;
+                }
             }
         }
         return groups;
