a9095e221bcc284da78d78d655cc401dfc27e139
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-03-12 17:15:20 +0530

    Time: 54 ms (6.58%) | Memory: 44.8 MB (37.29%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,22 +1,20 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        HashMap<Character , Integer> map = new HashMap<>();
-        map.put('a',0);
-        map.put('b',0);
-        map.put('c',0);
+        HashMap<Character , Integer > map = new HashMap<>();
+        map.put('a' , 0);
+        map.put('b' , 0);
+        map.put('c' , 0);
 
-        int left = 0 , count = 0;
+        int count = 0;
+        int left = 0;
 
-        //expanding the window
-        for ( int right = 0 ; right < s.length() ; right++){
-            map.put(s.charAt(right),map.get(s.charAt(right))+1);
+        for( int right = 0 ; right < s.length() ; right++){
+            map.put(s.charAt(right) , map.get(s.charAt(right)) + 1);
 
-            // checking if window is valid
             while(map.get('a') > 0 && map.get('b') > 0 && map.get('c') > 0){
-                //ading all valid strins to count
-                count += s.length() - right ;
+                count += s.length() - right;
 
-                map.put(s.charAt(left),map.get(s.charAt(left))-1); // Reduce freq from map
+                map.put(s.charAt(left) , map.get(s.charAt(left)) - 1);
                 left++;
             }
         }
