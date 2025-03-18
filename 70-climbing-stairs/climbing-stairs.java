b17e031cd79f7628c9ed2284753cb968ebfdb613
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-03-18 21:06:48 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (99.29%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -3,13 +3,13 @@ class Solution {
         if(n==0)return 1;
         if(n==1)return 1;
 
-        int prev1 = 1;
-        int prev2 = 1;
+        int prev1=1;
+        int prev2=1;
 
-        for(int i=2 ; i<=n ;i++){
+        for(int i = 2 ; i <= n ; i++){
             int current = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = current;
+            prev2=prev1;
+            prev1=current;
         }
         return prev1;
     }
