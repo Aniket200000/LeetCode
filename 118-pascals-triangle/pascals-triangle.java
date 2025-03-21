67057b49b8d561a2403719857bcf452190ec0040
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-03-21 09:42:27 +0530

    Time: 1 ms (84.03%) | Memory: 42.1 MB (59.48%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,21 +1,18 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> ans = new ArrayList<>();
         ans.add(new ArrayList<>());
-        ans.get(0).add(1); // First row is always [1]
+        ans.get(0).add(1);
 
-        for (int row = 1; row < numRows; row++) {
+        for(int row = 1 ; row < numRows ; row++){
             List<Integer> r = new ArrayList<>();
-            List<Integer> prevList = ans.get(row - 1);
+            List<Integer> prevList = ans.get(row-1);
 
-            r.add(1); // First element is always 1
-            for (int j = 1; j < row; j++) {
-                r.add(prevList.get(j - 1) + prevList.get(j)); // Sum of two above
+            r.add(1);
+            for(int j = 1 ; j < row ; j++){
+                r.add(prevList.get(j-1)+prevList.get(j));
             }
-            r.add(1); // Last element is always 1
+            r.add(1);
             ans.add(r);
         }
         return ans;
