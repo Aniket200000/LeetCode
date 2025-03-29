53d4b643beef86261a5dea8d64df138037a4a5c9
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-03-29 08:53:16 +0530

    Time: 12 ms (82.16%) | Memory: 63.2 MB (15.43%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -1,29 +1,27 @@
 class Solution {
     public int minimumIndex(List<Integer> nums) {
+        
         int n = nums.size();
-        int max = -1, count = 0;
+        int max = -1 , count = 0 ;
 
-        for (int num : nums) {
-            if (count == 0)
-                max = num;
+        for(int num : nums){
+            if(count == 0) max = num;
             count += (num == max) ? 1 : -1;
         }
 
-        int totalCount = 0;
-        for (int num : nums) {
-            if (num == max)
-                totalCount++;
+        int tCount = 0 ;
+        for(int num : nums){
+            if(num == max) tCount++;
         }
 
-        int left = 0, right = totalCount;
-
-        for (int i = 0; i < n - 1; i++) {
-            if (nums.get(i) == max) {
+        int left = 0 , right = tCount;
+        for(int i = 0 ; i < n-1 ; i++){
+            if(nums.get(i) == max){
                 left++;
                 right--;
             }
 
-            if (left * 2 > (i + 1) && right * 2 > (n - i - 1)) {
+            if(left * 2 > (i+1) && right * 2 > (n-i-1)){
                 return i;
             }
         }
