1e5f49e8fc21f1e02b9f097666308a46c9c57113
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-05-09 12:30:11 +0530

    Time: 2 ms (99.73%) | Memory: 41.3 MB (86.61%) - LeetSync

---
+++ 494-target-sum/target-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findTargetSumWays(int[] nums, int target) {
+        int total = 0;
+        for (int num : nums) total += num;
+
+        if ((target + total) % 2 != 0 || Math.abs(target) > total) {
+            return 0;
+        }
+
+        int subsetSum = (target + total) / 2;
+
+        int[] dp = new int[subsetSum + 1];
+        dp[0] = 1; 
+
+        for (int num : nums) {
+            for (int j = subsetSum; j >= num; j--) {
+                dp[j] += dp[j - num];
+            }
+        }
+
+        return dp[subsetSum];
+    }
+}
