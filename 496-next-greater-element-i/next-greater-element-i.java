b101eff7a0777ea804eb8d2082a1ab9eb4430448
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-05-12 00:31:34 +0530

    Time: 3 ms (90.33%) | Memory: 43.3 MB (81.16%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,31 +1,29 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        List<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < nums1.length; i++) {
             for (int j = 0; j < nums2.length; j++) {
                 if (nums1[i] == nums2[j]) {
-                    boolean found = false;
                     for (int k = j + 1; k < nums2.length; k++) {
                         if (nums2[k] > nums2[j]) {
                             list.add(nums2[k]);
-                            found = true;
                             break;
                         }
                     }
-                    if (!found) {
+                    if (list.size() <= i) {
                         list.add(-1);
                     }
-                    break; // stop scanning nums2 once match is found
+                    break;
                 }
             }
         }
 
-        // Convert list to array
         int[] ans = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             ans[i] = list.get(i);
         }
+
         return ans;
     }
 }
