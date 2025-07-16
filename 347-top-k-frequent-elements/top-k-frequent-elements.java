c16fdef8986cb08dab129e68de3e6ed9490d33fd
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-07-16 12:39:12 +0530

    Time: 14 ms (58.25%) | Memory: 48.9 MB (44.41%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,18 +1,16 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> map = new HashMap<>();
-
         for(int num : nums){
             map.put(num,map.getOrDefault(num,0)+1);
         }
-
         List<Map.Entry<Integer,Integer>> entryList = new ArrayList<>(map.entrySet());
-        entryList.sort((a,b) -> b.getValue() - a.getValue() );
+        entryList.sort((a,b) -> b.getValue() - a.getValue());
 
-        int[] freq = new int[k];
-        for(int i = 0 ; i<k ; i++){
-            freq[i] = entryList.get(i).getKey();
+        int[] result = new int[k];
+        for(int i = 0 ; i < k ; i++){
+            result[i] = entryList.get(i).getKey();
         }
-        return freq;
+        return result;
     }
 }
