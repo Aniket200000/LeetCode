6b19b44c0ba7e299fe30d414e1d58159edb7de1b
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-09-07 00:18:06 +0530

    Time: 43 ms (16.67%) | Memory: 101.4 MB (22.22%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.java
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.java
@@ -1,24 +1,31 @@
 class Solution {
     private long get(int num) {
-        long cnt = 0;
-        int i = 1;
-        int base = 1;
-        while (base <= num) {
-            int end = Math.min(base * 2 - 1, num);
-            cnt += (long) ((i + 1) / 2) * (end - base + 1);
-            i++;
-            base *= 2;
+        long total = 0;
+        int step = 1;
+        int start = 1;
+        
+        while (start <= num) {
+            int end = Math.min(start * 2 - 1, num);
+            long rangeLength = end - start + 1;
+            long countForStep = ((step + 1) / 2) * rangeLength;
+            
+            total += countForStep;
+            step++;
+            start *= 2;
         }
-        return cnt;
+        
+        return total;
     }
 
     public long minOperations(int[][] queries) {
-        long res = 0;
-        for (int[] q : queries) {
-            long count1 = get(q[1]);
-            long count2 = get(q[0] - 1);
-            res += (count1 - count2 + 1) / 2;
+        long operations = 0;
+        
+        for (int[] range : queries) {
+            long countEnd = get(range[1]);
+            long countStartMinusOne = get(range[0] - 1);
+            operations += (countEnd - countStartMinusOne + 1) / 2;
         }
-        return res;
+        
+        return operations;
     }
 }
