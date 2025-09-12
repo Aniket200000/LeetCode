6a3f7f59072d92c3ed5741211c651949c13dea91
Aniket Roy <120265447+Aniket200000@users.noreply.github.com>
2025-09-12 23:10:16 +0530

    Time: 23 ms (6.82%) | Memory: 45.8 MB (16.80%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+        return s
+            .chars()
+            .anyMatch(c -> {
+                return "aeiou".indexOf(c) != -1;
+            });
+    }
+}
